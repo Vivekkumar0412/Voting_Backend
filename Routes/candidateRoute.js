1d7bd126f8af963baa7a88108c4f2ad8b6a7d118
vivekkumar0412 <www.vivekkumar0412@gmail.com>
2025-02-22 17:44:08 +0530

    put and delete route in candidate

--- Routes/candidateRoute.js
+++ Routes/candidateRoute.js
@@ -7,8 +7,6 @@ const { verifyJwtTokenMiddleware } = require("../jwt");
 async function isAdmin(userId) {
   try {
     const user = await User.findById(userId);
-    console.log(user, "   is admin ");
-    console.log(user.role == "admin", "   testst");
     if (user.role == "admin") {
       return true;
     } else {
@@ -21,14 +19,12 @@ async function isAdmin(userId) {
 router.post("/", verifyJwtTokenMiddleware, async function (req, res) {
   try {
     if (await isAdmin(req.decodedData.id)) {
-      console.log("hiiiiii");
       const data = req.body;
       const response = await Candidate(data);
       await response.save();
       res.status(200).json({ msg: response });
     } else {
       return res.status(404).json({ msg: "only admin can create candidate!!" });
-      // res.status(200).json("hiii")
     }
   } catch (error) {
     res.status(404).json({ err: error });
